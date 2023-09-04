297f6861059b0a175454cf44d335ce7b4fc62ebe
Alex <borovinskihalex986@gmail.com>
2023-09-04 14:38:56 +0500

    course-work4

---
+++ src/main/java/ru/skypro/lessons/springboot/coursvork4/CoursVork4Application.java
@@ -0,0 +1,13 @@
+package ru.skypro.lessons.springboot.coursvork4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursVork4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursVork4Application.class, args);
+	}
+
+}
